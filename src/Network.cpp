501e45f3869e0a3df109d5ed45788e1c89421caf
shans96 <shanswanlow96@gmail.com>
2020-03-31 21:57:39 +0200

    Reserve memory for weights and biases

--- src/Network.cpp
+++ src/Network.cpp
@@ -13,6 +13,7 @@ void Network::generate_biases()
 	// Input layer has no biases, splice it
 	std::vector<int> temp = layers;
 	temp.erase(temp.begin());
+	biases.reserve(temp.size());
 	for (size_t i = 0; i < temp.size(); i++)
 	{
 		biases.push_back(Eigen::MatrixXd::Random(temp[i], 1));
@@ -22,6 +23,7 @@ void Network::generate_biases()
 void Network::generate_weights()
 {
 	std::vector<int>::iterator it;
+	weights.reserve(layers.size());
 	// Input layer has no weights, subtract 1
 	for (auto it = layers.begin(); it < layers.end() - 1; it++)
 	{
