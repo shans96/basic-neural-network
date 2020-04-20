59378ab1a7db06daecc963e376ff8ce6debea122
shans96 <shanswanlow96@gmail.com>
2020-04-20 21:52:50 +0200

    Add tests for backpropagation

--- test/test_network_processes.cpp
+++ test/test_network_processes.cpp
@@ -1,6 +1,8 @@
 #include "catch.hpp"
 #include "../src/Network.hpp"
 
+const double ACCURACY = 0.000001;
+
 std::vector<Eigen::MatrixXd> create_weights()
 {
 	Eigen::MatrixXd first_layer_weights(3, 2);
@@ -37,6 +39,45 @@ std::vector<Eigen::MatrixXd> create_biases()
 	return biases;
 }
 
+std::vector<Eigen::MatrixXd> create_small_backprop_weights()
+{
+	std::vector<Eigen::MatrixXd> expected_weights;
+
+	Eigen::MatrixXd first_weights(3,2);
+	Eigen::MatrixXd second_weights(1,3);
+
+	first_weights << -0.04111753, -0.02055877,
+		-0.00071989, -0.00035995,
+		-0.01446475, -0.00723237;
+
+	second_weights << -0.05737056, 
+		-0.00299714, 
+		-0.13232482;
+
+	expected_weights.push_back(first_weights);
+	expected_weights.push_back(second_weights);
+
+	return expected_weights;
+}
+
+std::vector<Eigen::MatrixXd> create_small_backprop_biases()
+{
+	std::vector<Eigen::MatrixXd> expected_biases;
+	Eigen::MatrixXd first_biases(3,1);
+	Eigen::MatrixXd second_biases(1,1);
+
+	first_biases << -0.02055877,
+		-0.00035995,
+		-0.00723237;
+
+	second_biases << -0.14394565;
+
+	expected_biases.push_back(first_biases);
+	expected_biases.push_back(second_biases);
+
+	return expected_biases;
+}
+
 Network create_test_network()
 {
 	std::vector<int> layers = { 2,3,1 };
@@ -46,11 +87,30 @@ Network create_test_network()
 	return Network(layers, weights, biases);
 }
 
-
 TEST_CASE("Feed forward produces the correct (approximate) answers", "[feedforward]")
 {
 	Network network = create_test_network();
 	double network_feed_forward_result = std::get<0>(network.feed_forward(Eigen::Vector2d(2.0, 1.0)))(0);
 	WARN("Network feed forward output: " << network_feed_forward_result << ". Expecting approximately 0.27045343.");
-	REQUIRE(network_feed_forward_result == Approx(0.27045343).epsilon(0.000001));
+	REQUIRE(network_feed_forward_result == Approx(0.27045343).epsilon(ACCURACY));
+}
+
+TEST_CASE("Backpropagation produces the correct (approximate) answers across all neurons", "[backpropagation]")
+{
+	Network network = create_test_network();
+	Eigen::MatrixXd expected_output(1, 1);
+	expected_output << 1;
+
+	std::pair<std::vector<Eigen::MatrixXd>, std::vector<Eigen::MatrixXd>> backpropagation_result = network.backpropagate(Eigen::Vector2d(2.0, 1.0), expected_output);
+
+	std::vector<Eigen::MatrixXd> expected_weights = create_small_backprop_weights();
+	std::vector<Eigen::MatrixXd> expected_biases = create_small_backprop_biases();
+
+	std::vector<Eigen::MatrixXd> calculated_biases = std::get<0>(backpropagation_result);
+	std::vector<Eigen::MatrixXd> calculated_weights = std::get<1>(backpropagation_result);
+
+	REQUIRE(calculated_biases[0].isApprox(expected_biases[0], ACCURACY));
+	REQUIRE(calculated_biases[1].isApprox(expected_biases[1], ACCURACY));
+	REQUIRE(calculated_weights[0].isApprox(expected_weights[0], ACCURACY));
+	REQUIRE(calculated_weights[1].isApprox(expected_weights[1], ACCURACY));
 }
