a1a0df85a06934d40e53f07483382bc7b30f8973
shans96 <shanswanlow96@gmail.com>
2020-04-22 18:20:56 +0200

    Remove unused method

--- src/network_calc.h
+++ src/network_calc.h
@@ -5,7 +5,6 @@
 namespace network_calc 
 {
 	Eigen::MatrixXd multiply_matrices(Eigen::MatrixXd matrix1, Eigen::MatrixXd matrix2);
-	double calculate_error(Eigen::MatrixXd expected, Eigen::MatrixXd calculated);
 	double sigmoid(double x);
 	double sigmoid_derivative(double x);
 	double result_difference(double actual, double predicted);
